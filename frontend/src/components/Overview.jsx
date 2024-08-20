10f927ef5f852712bda1d7b0d166be25d3ff5af3
Dwarika Kumar <dwarikabppimt202249@gmail.com>
2024-08-20 17:47:16 +0530

    text size increased - DashboardLayout

--- frontend/src/components/Overview.jsx
+++ frontend/src/components/Overview.jsx
@@ -15,7 +15,7 @@ const Overview = () => {
 
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-4">Progress</h2>
+      <h2 className="text-3xl text-indigo-700 font-bold mb-10">Progress</h2>
       <div className="bg-white p-6 rounded-lg shadow-md mb-4">
         <h3 className="text-xl font-semibold mb-3">Progress Summary</h3>
         <ul>
